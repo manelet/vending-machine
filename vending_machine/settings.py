e9cb9a813c09b35712aa9f50af8d6b31d51ca68c
Manel Escuer <manelet@gmail.com>
2023-09-04 17:34:07 +0200

    fix static files vercel

--- vending_machine/settings.py
+++ vending_machine/settings.py
@@ -11,11 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
-import os.path
-import sys
-
-PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,9 +130,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(PROJECT_ROOT, "static")
-STATIC_URL = "/static/"
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- vending_machine/urls.py
+++ vending_machine/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from apps.health.views import healthcheck
 import apps.vending.views as vending_views
@@ -25,3 +27,6 @@ urlpatterns = [
     path("products/", vending_views.Products.as_view()),
     path("customers/", vending_views.Customer.as_view()),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
