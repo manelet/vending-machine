4ce7690b49d64d57d44de9351299a0d7299e7675
Manel Escuer <manelet@gmail.com>
2023-09-04 17:46:33 +0200

    more tests

--- vending_machine/settings.py
+++ vending_machine/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
