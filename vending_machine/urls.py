98f7cdbdaf6afeeea2287df8dd8219a86723b310
Manel Escuer <manelet@gmail.com>
2023-09-04 18:25:31 +0200

    add default html page

--- vending_machine/urls.py
+++ vending_machine/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import re_path, path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.generic import TemplateView
 from apps.health.views import healthcheck
 import apps.vending.views as vending_views
 
@@ -26,6 +26,7 @@ urlpatterns = [
     path("healthcheck/", healthcheck),
     path("products/", vending_views.Products.as_view()),
     path("customers/", vending_views.Customer.as_view()),
+    re_path(r"^$", TemplateView.as_view(template_name="root.html")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
