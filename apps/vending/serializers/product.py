7319b24b5b2637762ef17cedaa2cdb8ec291c816
Manel Escuer <manelet@gmail.com>
2023-09-05 17:17:02 +0200

    add images to results

--- apps/vending/serializers/product.py
+++ apps/vending/serializers/product.py
@@ -4,3 +4,4 @@ from rest_framework import serializers
 class ProductSerializer(serializers.Serializer):
     name = serializers.CharField()
     price = serializers.DecimalField(max_digits=4, decimal_places=2)
+    image = serializers.CharField()
