300f5fa82ba9e830650aaf1b1ae06be1ed26a7ee
Manel Escuer <manelet@gmail.com>
2023-09-05 17:06:09 +0200

    changes

--- apps/vending/models/product.py
+++ apps/vending/models/product.py
@@ -13,8 +13,9 @@ class Product(models.Model):
     price = models.DecimalField(
         max_digits=4, decimal_places=2, validators=[MinValueValidator(Decimal("0.00"))]
     )
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    updated_at = models.DateTimeField(null=True, blank=True)
+    image = models.URLField(null=True, blank=True)
 
     def __str__(self):
         return self.name
