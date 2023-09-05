fb46c341b59b78193fb6fa048e6a2a3060b1a890
Manel Escuer <manelet@gmail.com>
2023-09-05 17:03:08 +0200

    allo posts requests

--- apps/vending/models/product.py
+++ apps/vending/models/product.py
@@ -15,3 +15,6 @@ class Product(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return self.name
